45125547bceb367aad2c4a642c8edd69ef039360
“Anna <anna35.anna35.v@gmail.com>
2024-02-20 00:08:59 +0300

    pro

---
+++ js/script.js
@@ -0,0 +1,15 @@
+"use strict"
+
+const nameProduct = 'Яблоки';
+const quantityOfGoods = 4;
+const productCategory = 'Фрукты';
+const priceProduct = 99;
+const sum = priceProduct*quantityOfGoods ;
+
+console.log(nameProduct);
+console.log(quantityOfGoods);
+console.log(productCategory);
+console.log(priceProduct);
+console.log(sum)
+
+
